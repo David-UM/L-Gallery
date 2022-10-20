b831f93518bcc021b5aae640aefc6cde35646dff
David Urbano <david.urbano_code@outlook.com>
2022-10-19 20:09:25 -0500

    adding fake data for test

---
+++ src/data/FakeData.js
@@ -0,0 +1,29 @@
+const Data = [
+    {
+        img:1,
+        mood: "angry",
+        appear: true
+    },
+    {
+        img:2,
+        mood: "Jin",
+        appear: true
+    },
+    {
+        img:3,
+        mood:"neutral",
+        appear: true
+    },
+    {
+        img:4,
+        mood: "neutral",
+        appear: true
+    },
+    {
+        img:5,
+        mood:"shooked",
+        appear: true
+    }
+]
+
+export default Data;
