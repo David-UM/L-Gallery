a9b76513b96f0bb4407f89b3bb5d7d9594901402
David Urbano <david.urbano_code@outlook.com>
2022-10-19 20:16:05 -0500

    First search bar working/ with map and filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
+import Myheader from "./components/Myheader"
+import FakeData from "./data/FakeData.js"
+import Photo  from "./components/Photo"
+import a from "./assets/Photos/Photo-1.jpeg"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [state, setState] = useState("")
+
+  const handleOnChange = (e) => {
+    return setState(state = e.target.value)
+  };
+
+  /* const PhotoList =  */
 
   return (
     <div className="App">
-      
+       <h1>{state}</h1>
+       <input className="FirstSearchBar" type="text" onChange={handleOnChange}/>
+        {
+          state === ""?FakeData.map((photo) => <Photo number={photo.img.toString()} alt={photo.mood} key={photo.img.toString()}/>):FakeData.filter(photo => photo.mood === state).map((photo) => <Photo number={photo.img.toString()} alt={photo.mood} key={photo.img.toString()}/>)
+  
+            
+          
+        }
+        
     </div>
   )
 }
