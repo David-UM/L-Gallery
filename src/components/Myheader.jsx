9049244e4342249c7fe71ac966bab7b5edf6f3f7
David Urbano <david.urbano_code@outlook.com>
2022-10-21 01:30:20 -0500

    V 1.25

--- src/components/Myheader.jsx
+++ src/components/Myheader.jsx
@@ -5,7 +5,7 @@ const MyHeader = () => {
     return (
         <>
             <div className="myheader-container">
-                <h1 className="myheader-title">Pumpkin patch</h1>
+                <h1 className="myheader-title">Pumpkin PATCH</h1>
             </div>
         </>
     )
