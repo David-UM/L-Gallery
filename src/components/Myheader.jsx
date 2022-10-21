78943b323f8ae40513e3398e90cf9ef2cb6b1f96
David Urbano <david.urbano_code@outlook.com>
2022-10-21 01:21:23 -0500

    V 1.20

--- src/components/Myheader.jsx
+++ src/components/Myheader.jsx
@@ -5,7 +5,7 @@ const MyHeader = () => {
     return (
         <>
             <div className="myheader-container">
-                <h1 className="myheader-title">Pumpkin's Gallery</h1>
+                <h1 className="myheader-title">Pumpkin patch</h1>
             </div>
         </>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = ({handle}) => {
     return (
         <>
         <div className="SearchBar-container">
-             <input className="SearchBar-bar" type="text" onChange={handle} placeholder="Write something"/>
+             <input className="SearchBar-bar" type="text" onChange={handle} placeholder="Search..."/>
         </div>
             
         </>
