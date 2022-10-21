f3321cee0ed5531fa2612a9c20359ce169e859dc
David Urbano <david.urbano_code@outlook.com>
2022-10-20 19:53:10 -0500

    V 1.10

--- src/components/MyGallery.jsx
+++ src/components/MyGallery.jsx
@@ -5,15 +5,17 @@ import "./../styles/MyGallery.css"
 const MyGallery = ({estado}) => {
     return (
         <>
+
             <div className="MyGallery-container">
             {
                 estado === ""?FakeData.map((photo) => 
                 <Photo number={photo.img.toString()} alt={photo.mood} key={photo.img.toString()}/>
                 )
-                :FakeData.filter(photo => photo.mood === estado).map((photo) => 
+                :FakeData.filter(photo => photo.mood.toLowerCase() === estado).map((photo) => 
                 <Photo number={photo.img.toString()} alt={photo.mood} key={photo.img.toString()}/>
                 )  
-            }    
+            }
+               
             </div>
         </>
     );
--- src/data/FakeData.js
+++ src/data/FakeData.js
@@ -1,28 +1,55 @@
 const Data = [
     {
         img:1,
-        mood: "angry",
-        appear: true
+        mood: "Jin",
     },
     {
         img:2,
         mood: "Jin",
-        appear: true
     },
     {
         img:3,
-        mood:"neutral",
-        appear: true
+        mood:"Jin",
     },
     {
         img:4,
-        mood: "neutral",
-        appear: true
+        mood: "Yuno",
     },
     {
         img:5,
-        mood:"shooked",
-        appear: true
+        mood:"Yuno",
+    },
+    {
+        img:6,
+        mood:"Yuno",
+    },
+    {
+        img:7,
+        mood:"Papi",
+    },
+    {
+        img:8,
+        mood:"Jin",
+    },
+    {
+        img:9,
+        mood:"Jin",
+    },
+    {
+        img:10,
+        mood:"Yuno",
+    },
+    {
+        img:11,
+        mood:"Yuno",
+    },
+    {
+        img:12,
+        mood:"Yuno",
+    },
+    {
+        img:13,
+        mood:"Yuno",
     }
 ]
 
