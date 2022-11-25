8cdfc919b3d066df807cd882dc762946aa55b9ee
David Urbano <david.urbano_code@outlook.com>
2022-11-25 00:59:27 -0500

    Creando funcionalidad para abrir y cerrar imagenes

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,15 +1,36 @@
 import './../styles/photo.css';
+import { useState } from 'react';
+
 /* Componente que renderiza cada foto y recibe dos propiedades que vienen del componente PhotoGallery.jsx */
 const Photo = ({ index, description }) => {
+	let [containerclass, setContainerclass] = useState('Photo-container');
+	let [imageclass, setImageclass] = useState('Photo');
+
+	const handleClickOpen = () => {
+		setContainerclass((containerclass = 'grow-container'));
+		setImageclass((imageclass = 'grow-photo'));
+		document.body.style.overflow = 'hidden';
+	};
+
+	const handleClickClose = () => {
+		setContainerclass((containerclass = 'Photo-container'));
+		setImageclass((imageclass = 'Photo'));
+		document.body.style.overflow = 'visible';
+	};
 	return (
-		<div className='Photo-container'>
+		<div className={containerclass}>
 			<img
 				src={`https://raw.githubusercontent.com/David-UM/L-Gallery/main/src/assets/Photos/Photo-${index}.jpeg`}
 				alt={description}
 				key={index}
-				className='Photo'
+				className={imageclass}
 			/>
-			<button className='photo-button hidden'>See full photo</button>
+			<button className='photo-buttonOpen' onClick={handleClickOpen}>
+				See full photo
+			</button>
+			<button className='photo-buttonClose' onClick={handleClickClose}>
+				X
+			</button>
 		</div>
 	);
 };
